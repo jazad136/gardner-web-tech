2541c8583d52cfb1ef97f2174be8b38d65f47fbc
Adam Gardner <adga2012@gmail.com>
2022-01-27 20:10:22 -0700

    Add: logging to magic client

--- packages/ui/auth/Magic.tsx
+++ packages/ui/auth/Magic.tsx
@@ -1,14 +1,25 @@
 import { Magic } from "magic-sdk";
 import { OAuthExtension } from "@magic-ext/oauth";
+import * as logger from "npmlog";
+
+let magic: any = null;
+logger.enableColor();
 
 // Create client-side Magic instance
 const createMagicClient = (key: string) => {
-  return (
-    typeof window != "undefined" &&
-    new Magic(key, {
-      extensions: [new OAuthExtension()],
-    })
-  );
+  if (typeof window === "undefined") {
+    logger.error(
+      `Magic:createMagicClient`,
+      "Window is required to create magic client instance."
+    );
+    return null;
+  }
+
+  magic = new Magic(key, {
+    extensions: [new OAuthExtension()],
+  });
+
+  return magic;
 };
 
 export const magicClient = (key: string) => createMagicClient(key);
