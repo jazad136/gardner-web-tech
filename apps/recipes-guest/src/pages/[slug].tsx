8852b30a7d5b223a39ed9943107e7530dda1cf4b
Adam Gardner <adga2012@gmail.com>
2022-01-31 23:06:51 -0700

    remove unused import

--- apps/recipes-guest/src/pages/[slug].tsx
+++ apps/recipes-guest/src/pages/[slug].tsx
@@ -1,4 +1,4 @@
-import { createRef, useCallback, useEffect, useState } from "react";
+import { createRef, useEffect, useState } from "react";
 import Image from "next/image";
 import ErrorPage from "next/error";
 import { useRouter } from "next/router";
