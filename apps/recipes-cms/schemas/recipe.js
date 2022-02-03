1ef3db66675b10932b08b3fa917bc479e3aeba28
Adam Gardner <adga2012@gmail.com>
2022-02-02 22:10:13 -0700

    add "serves" to cms

--- apps/recipes-cms/schemas/recipe.js
+++ apps/recipes-cms/schemas/recipe.js
@@ -1,142 +1,153 @@
-import {
-  MdCake as icon
-} from 'react-icons/md'
+import { MdCake as icon } from "react-icons/md";
 
 export default {
-  name: 'recipe',
-  title: 'Recipe',
-  type: 'document',
+  name: "recipe",
+  title: "Recipe",
+  type: "document",
   icon,
-  fields: [{
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-      validation: Rule => Rule.required().min(3).max(80),
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required().min(3).max(80),
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 100,
       },
     },
     {
-      name: 'notes',
-      title: 'Notes',
-      type: 'array',
-      of: [{
-        type: 'block'
-      }]
+      name: "notes",
+      title: "Notes",
+      type: "array",
+      of: [
+        {
+          type: "block",
+        },
+      ],
     },
     {
-      name: 'cookTime',
-      title: 'Cook Time',
-      type: 'number',
-      validation: Rule => Rule.required().min(1),
+      name: "serves",
+      title: "Serves",
+      type: "number",
+      initialValue: 1,
+      validation: (Rule) => Rule.required().min(1),
     },
     {
-      name: 'prepTime',
-      title: 'Prep Time',
-      type: 'number',
-      validation: Rule => Rule.required().min(1),
+      name: "cookTime",
+      title: "Cook Time",
+      type: "number",
+      validation: (Rule) => Rule.required().min(1),
     },
     {
-      name: 'restTime',
-      title: 'Rest/Marinade Time',
-      type: 'string',
+      name: "prepTime",
+      title: "Prep Time",
+      type: "number",
+      validation: (Rule) => Rule.required().min(1),
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "restTime",
+      title: "Rest/Marinade Time",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
     {
-      name: 'youTubeUrls',
-      title: 'YouTube Urls',
-      type: 'array',
-      of: [{
-        type: 'string'
-      }],
+      name: "youTubeUrls",
+      title: "YouTube Urls",
+      type: "array",
+      of: [
+        {
+          type: "string",
+        },
+      ],
     },
     {
-      name: 'ingredients',
-      title: 'Ingredients',
-      type: 'array',
-      of: [{
-        type: 'ingredient'
-      }],
-      validation: Rule => Rule.required().unique().min(1),
+      name: "ingredients",
+      title: "Ingredients",
+      type: "array",
+      of: [
+        {
+          type: "ingredient",
+        },
+      ],
+      validation: (Rule) => Rule.required().unique().min(1),
     },
     {
-      name: 'instructions',
-      title: 'Instructions',
-      type: 'array',
-      of: [{
-        type: 'block',
-        lists: [{
-          title: 'Numbered',
-          value: 'number'
-        }],
-        styles: [{
-            title: 'Normal',
-            value: 'normal'
-          },
-          {
-            title: 'H1',
-            value: 'h1'
-          },
-          {
-            title: 'H2',
-            value: 'h2'
-          }
-        ],
-        marks: {
-          decorators: [{
-              title: "Strong",
-              value: 'strong',
+      name: "instructions",
+      title: "Instructions",
+      type: "array",
+      of: [
+        {
+          type: "block",
+          lists: [
+            {
+              title: "Numbered",
+              value: "number",
+            },
+          ],
+          styles: [
+            {
+              title: "Normal",
+              value: "normal",
             },
             {
-              title: "Emphasis",
-              value: 'em',
+              title: "H1",
+              value: "h1",
             },
-          ]
-        }
-      }],
-      validation: Rule => Rule.required(),
+            {
+              title: "H2",
+              value: "h2",
+            },
+          ],
+          marks: {
+            decorators: [
+              {
+                title: "Strong",
+                value: "strong",
+              },
+              {
+                title: "Emphasis",
+                value: "em",
+              },
+            ],
+          },
+        },
+      ],
+      validation: (Rule) => Rule.required(),
     },
   ],
   preview: {
     select: {
-      title: 'title',
-      image: 'image',
-      prep: 'prepTime',
-      cook: 'cookTime',
-      rest: 'restTime'
+      title: "title",
+      image: "image",
+      prep: "prepTime",
+      cook: "cookTime",
+      rest: "restTime",
     },
-    prepare({
-      title,
-      image,
-      prep,
-      cook,
-      rest
-    }) {
+    prepare({ title, image, prep, cook, rest }) {
       let subtitle = `Time to make: ${prep + cook} min`;
 
       if (rest) {
-        subtitle += ` (+${rest})`
+        subtitle += ` (+${rest})`;
       }
 
       return {
         title,
         subtitle,
-        media: image
-      }
-    }
+        media: image,
+      };
+    },
   },
-
-}
+};
