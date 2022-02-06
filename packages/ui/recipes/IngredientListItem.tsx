1b947e69802d306d6649321845ea9c188d1b79b5
Adam Gardner <adga2012@gmail.com>
2022-02-05 23:29:14 -0700

    3 columns with paragraph component

--- packages/ui/recipes/IngredientListItem.tsx
+++ packages/ui/recipes/IngredientListItem.tsx
@@ -1,19 +1,18 @@
 import { Ingredient } from ".";
+import { Paragraph } from "..";
 
 export interface IngredientListItemProps {
   ingredient: Ingredient;
 }
 
 export const IngredientListItem = ({ ingredient }: IngredientListItemProps) => (
-  <li className="w-full md:w-1/2 mb-6">
-    <div className="prose dark:prose-dark flex flex-col-reverse sm:flex-row">
-      <span className="prose dark:prose-dark mr-4 italic">
+  <li className="w-full md:w-1/2 lg:w-1/3">
+    <Paragraph>
+      <div className="font-bold">{ingredient.title}</div>
+      <div>
         {ingredient.quantity || ""} {ingredient.unit || ""}
-      </span>
-      <span className="prose dark:prose-dark font-bold whitespace-nowrap">
-        {ingredient.title}
-      </span>
-    </div>
-    <div className="prose dark:prose-dark text-sm">{ingredient.notes}</div>
+      </div>
+      <div className="prose-sm italic">{ingredient.notes}</div>
+    </Paragraph>
   </li>
 );
