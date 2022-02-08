3f4f31d93ee4130bf37da66c2aa299e0d4b98710
Adam Gardner <adga2012@gmail.com>
2022-02-07 22:44:23 -0700

    fix searchbar text color

--- packages/ui/recipes/RecipeSearchForm.tsx
+++ packages/ui/recipes/RecipeSearchForm.tsx
@@ -8,7 +8,7 @@ export const RecipeSearchForm = ({ setSearch }: RecipeSearchFormProps) => (
   <form className="flex w-full px-4 py-2">
     <input
       type="search"
-      className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      className="prose dark:prose-dark shadow appearance-none border rounded-lg w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
       id="search"
       placeholder="Search"
       onChange={(e) => setSearch(e.target.value)}
