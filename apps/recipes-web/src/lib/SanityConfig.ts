250e357f9e133bdece1da5ac074c72d404303136
Adam Gardner <adga2012@gmail.com>
2022-02-08 11:04:46 -0700

    fix useCdn messages

--- apps/recipes-web/src/lib/SanityConfig.ts
+++ apps/recipes-web/src/lib/SanityConfig.ts
@@ -4,6 +4,6 @@ export const config: ClientConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2022-01-27",
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 };
