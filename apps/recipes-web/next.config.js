4855a44424529f939a2be50ca6ff7e0ccd29b2b8
Adam Gardner <adga2012@gmail.com>
2022-07-08 14:45:10 -0600

    chore: add swcMinify to next config

--- apps/recipes-web/next.config.js
+++ apps/recipes-web/next.config.js
@@ -3,6 +3,7 @@ const withTM = require("next-transpile-modules")(["ui"]);
 /** @type {import('next').NextConfig} */
 module.exports = withTM({
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["cdn.sanity.io"],
   },
