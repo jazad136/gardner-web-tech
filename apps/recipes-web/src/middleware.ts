a9af48c891b96beebe021c863c9f72c8aabaae78
Adam Gardner <adga2012@gmail.com>
2022-08-21 22:09:48 -0600

    fix(middleware): remove matcher

--- apps/recipes-web/src/middleware.ts
+++ apps/recipes-web/src/middleware.ts
@@ -1,7 +1,6 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/", "/recipe/:path*", "/_next/data/:path*"],
   pages: {
     signIn: "/auth/login",
     error: "/auth/error",
