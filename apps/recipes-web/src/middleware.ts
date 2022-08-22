23953b98404831afa74fcb8b69be6c08f5f9fa63
Adam Gardner <adga2012@gmail.com>
2022-08-21 22:07:20 -0600

    fix(middleware): add pages to config

--- apps/recipes-web/src/middleware.ts
+++ apps/recipes-web/src/middleware.ts
@@ -2,4 +2,9 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/", "/recipe/:path*", "/_next/data/:path*"],
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+  },
 };
