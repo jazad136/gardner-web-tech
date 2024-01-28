967dfa6760555daa82e803c58bb9071c4029aa4e
Adam Gardner <adga2012@gmail.com>
2024-01-28 14:47:37 -0700

    remove auth temporarily

--- apps/recipes-web/src/middleware.ts
+++ apps/recipes-web/src/middleware.ts
@@ -1,10 +1,10 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/auth/login",
-    error: "/auth/error",
-    verifyRequest: "/auth/verify-request",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+// export default withAuth({
+//   pages: {
+//     signIn: "/auth/login",
+//     error: "/auth/error",
+//     verifyRequest: "/auth/verify-request",
+//   },
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
